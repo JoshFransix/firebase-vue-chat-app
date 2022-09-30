f55886056a3156c3a05097cfb2bf96823737fef6
JoshFransix <joshiandersonk69@gmail.com>
2022-09-30 19:05:43 +0100

    switched up interface

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div
+  <div class="main">
+    <!-- <div
       class="view login"
       v-if="state.username === '' || state.username === null"
     >
@@ -16,11 +16,29 @@
           <input type="submit" value="Login" />
         </div>
       </form>
+    </div> -->
+    <div
+      v-if="state.username === '' || state.username === null"
+      class="container"
+    >
+      <div class="form-container">
+        <form @submit.prevent="Login">
+          <p>Welcome</p>
+          <input
+            type="text"
+            v-model="inputUsername"
+            placeholder="Please enter your username..."
+          /><br />
+          <input type="password" placeholder="Password" /><br />
+          <input type="submit" value="Sign in" /><br />
+          <!-- <a href="#">Forgot Password?</a> -->
+        </form>
+      </div>
     </div>
-    <div v-else class="view chat">
+    <div v-else class="chat">
       <header>
-        <button class="logout" @click="Logout">Logout</button>
         <h1>Welcome, {{ state.username }}</h1>
+        <button class="logout" @click="Logout">Logout</button>
       </header>
       <section class="chat-box">
         <div
@@ -123,7 +141,9 @@ export default {
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+
 * {
+  position: relative;
   font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -131,119 +151,188 @@ export default {
   padding: 0;
   box-sizing: border-box;
   transition: all 0.3s ease-in-out;
-}
-.view {
-  display: flex;
-  justify-content: center;
-  min-height: 100vh;
-  background-color: #ea526f;
 
-  &.login {
+  .container {
+    display: flex;
+    justify-content: center;
+    min-height: 100vh;
     align-items: center;
-    .login-form {
-      display: block;
-      width: 60%;
-      padding: 15px;
+    background: linear-gradient(to left, #283593, #1976d2);
 
-      .form-inner {
-        display: block;
-        background-color: #fff;
-        padding: 50px 15px;
-        border-radius: 16px;
-        box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
-        h1 {
-          color: #aaa;
-          font-size: 28px;
-          margin-bottom: 30px;
-        }
-        label {
-          display: block;
-          margin-bottom: 5px;
-          color: #aaa;
-          font-size: 16px;
-          transition: 0.4s;
-        }
-        input[type="text"] {
-          appearance: none;
-          border: none;
-          outline: none;
-          background: none;
-          display: block;
-          width: 100%;
-          padding: 10px 15px;
-          border-radius: 8px;
-          margin-bottom: 15px;
+    .form-container {
+      width: 100%;
+      position: relative;
+      form {
+        margin: auto;
+        background: rgba(255, 255, 255, 0.06);
+        padding: 2em;
+        width: 30%;
+        height: max-content;
+        border-radius: 20px;
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        -webkit-backdrop-filter: blur(10px);
+        backdrop-filter: blur(10px);
+        box-shadow: 20px 20px 40px -6px rgba(0, 0, 0, 0.2);
+        text-align: center;
+        position: relative;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
 
-          color: #333;
-          font-size: 18px;
-          box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-          background-color: #f3f3f3;
-          transition: 0.4s;
-          &::placeholder {
-            color: #888;
-            transition: 0.4s;
-          }
-        }
-        input[type="submit"] {
-          appearance: none;
-          border: none;
-          outline: none;
-          background: none;
-          display: block;
-          width: 100%;
-          padding: 10px 15px;
-          background-color: #ea526f;
-          border-radius: 8px;
+        p {
+          font-weight: 500;
           color: #fff;
-          font-size: 18px;
-          font-weight: 700;
+          opacity: 0.7;
+          font-size: 1.4rem;
+          margin-top: 0;
+          margin-bottom: 60px;
+          text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
         }
-        &:focus-within {
-          label {
-            color: #ea526f;
+        a {
+          text-decoration: none;
+          color: #ddd;
+          font-size: 12px;
+          &:hover {
+            text-shadow: 2px 2px 6px #00000040;
           }
-          input[type="text"] {
-            background-color: #fff;
-            box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
-            &::placeholder {
-              color: #666;
-            }
+          &:active {
+            text-shadow: none;
+          }
+        }
+
+        input {
+          background: transparent;
+          width: 100%;
+          padding: 1em;
+          margin-bottom: 2em;
+          border: none;
+          border-left: 1px solid rgba(255, 255, 255, 0.3);
+          border-top: 1px solid rgba(255, 255, 255, 0.3);
+          border-radius: 5000px;
+          -webkit-backdrop-filter: blur(5px);
+          backdrop-filter: blur(5px);
+          box-shadow: 4px 4px 60px rgba(0, 0, 0, 0.2);
+          color: #fff;
+          font-family: Montserrat, sans-serif;
+          font-weight: 500;
+          -webkit-transition: all 0.2s ease-in-out;
+          transition: all 0.2s ease-in-out;
+          text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+          &:hover {
+            background: rgba(255, 255, 255, 0.1);
+            box-shadow: 4px 4px 60px 8px rgba(0, 0, 0, 0.2);
+          }
+          &[type="text"]:focus,
+          &[type="password"]:focus {
+            background: rgba(255, 255, 255, 0.1);
+            box-shadow: 4px 4px 60px 8px rgba(0, 0, 0, 0.2);
+          }
+          &[type="submit"] {
+            margin-top: 10px;
+            width: 40%;
+            font-size: 1rem;
+          }
+          &[type="submit"]:hover {
+            cursor: pointer;
+          }
+          &[type="submit"]:active {
+            background: rgba(255, 255, 255, 0.2);
           }
         }
       }
+    }
+
+    ::-webkit-input-placeholder {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      color: #fff;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    }
+
+    ::-moz-placeholder {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      color: #fff;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    }
+
+    :-ms-input-placeholder {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      color: #fff;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    }
+
+    ::-ms-input-placeholder {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      color: #fff;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    }
+
+    ::placeholder {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      color: #fff;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+    }
+
+    a,
+    input:focus,
+    select:focus,
+    textarea:focus,
+    button:focus {
+      outline: none;
     }
   }
-  &.chat {
+
+  .chat {
+    display: flex;
+    justify-content: center;
+    min-height: 100vh;
     flex-direction: column;
+    background: linear-gradient(to left, #283593, #1976d2);
+
     header {
       position: relative;
-      display: block;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
       width: 100%;
-      padding: 50px 30px 10px;
+      padding: 20px;
+
       .logout {
-        position: absolute;
-        top: 15px;
-        right: 15px;
+        outline: none;
         appearance: none;
         border: none;
-        outline: none;
-        background: none;
-
+        background: rgba(255, 255, 255, 0.06);
+        padding: 0.5rem 1.8rem;
+        border-radius: 10px;
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        cursor: pointer;
         color: #fff;
         font-size: 18px;
-        margin-bottom: 10px;
-        text-align: right;
+
+        &:hover {
+          background: rgba(255, 255, 255, 0.1);
+          box-shadow: 4px 4px 60px 8px rgba(0, 0, 0, 0.2);
+        }
       }
+
       h1 {
         color: #fff;
+        font-size: 18px;
       }
     }
+
     .chat-box {
       border-radius: 24px 24px 0px 0px;
       background-color: #fff;
       box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
       flex: 1 1 100%;
       padding: 30px;
+
       .message {
         display: flex;
         margin-bottom: 15px;
@@ -256,6 +345,7 @@ export default {
             padding-left: 15px;
             padding-right: 15px;
           }
+
           .content {
             display: inline-block;
             padding: 10px 20px;
@@ -267,29 +357,35 @@ export default {
             text-align: left;
           }
         }
+
         &.current-user {
           margin-top: 30px;
           justify-content: flex-end;
           text-align: right;
+
           .message-inner {
             max-width: 75%;
+
             .content {
               color: #fff;
               font-weight: 600;
-              background-color: #ea526f;
+              background: linear-gradient(to left, #283593, #1976d2);
             }
           }
         }
       }
     }
+
     footer {
       position: sticky;
       bottom: 0px;
       background-color: #fff;
       padding: 30px;
       box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
+
       form {
         display: flex;
+
         input[type="text"] {
           flex: 1 1 100%;
           appearance: none;
@@ -306,6 +402,7 @@ export default {
           box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
           background-color: #f3f3f3;
           transition: 0.4s;
+
           &::placeholder {
             color: #888;
             transition: 0.4s;
@@ -316,11 +413,11 @@ export default {
           appearance: none;
           border: none;
           outline: none;
-          background: none;
           display: block;
+          cursor: pointer;
           padding: 10px 15px;
           border-radius: 0px 8px 8px 0px;
-          background-color: #ea526f;
+          background: linear-gradient(to left, #283593, #1976d2);
           color: #fff;
           font-size: 18px;
           font-weight: 700;
@@ -330,11 +427,32 @@ export default {
   }
 }
 
-@media screen and (max-width: 540px) {
-  .view {
-    &.login {
-      .login-form {
-        width: 100%;
+@media screen and (max-width: 1240px) {
+  .container {
+    .form-container {
+      form {
+        width: 50%;
+
+        input {
+          &[type="submit"] {
+            width: 30%;
+          }
+        }
+      }
+    }
+  }
+}
+@media screen and (max-width: 740px) {
+  .container {
+    .form-container {
+      form {
+        width: 90%;
+
+        input {
+          &[type="submit"] {
+            width: 60%;
+          }
+        }
       }
     }
   }
